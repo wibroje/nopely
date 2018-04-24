cfd766ebb44d57035bbb2c8f6de4fe962e89dd72
William Jefferies <willjefferies10@gmail.com>
2018-04-24 12:41:37 -0600

    Setup push to github part 2

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/nopely");
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
-if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.MLAB_URL)
-} else {
-	mongoose.connect("mongodb://localhost/nopely");
-}
+// middleware
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost/simple-login');
+
+app.get('/', function (req,res) {
+	res.render('index');
+})
+
+
+app.get('/signup', function (req, res) {
+  res.send('signup coming soon');
+});
+
+
+app.get('/login', function (req, res) {
+  res.send('login coming soon');
+});
+
+// listen
+app.listen(3000, function () {
+  console.log('server started on locahost:3000');
+});
