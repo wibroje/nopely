94c520bb4259e6deecc75580b96d36e443554934
William Jefferies <willjefferies10@gmail.com>
2018-04-24 12:36:01 -0600

    Setup push to github

---
+++ server.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/nopely");
+
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+	mongoose.connect("mongodb://localhost/nopely");
+}
